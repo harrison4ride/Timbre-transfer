630632046fd7f5be6fa9d590dcd914f2d60c18f6
harrison4anewbe <18622245010@163.com>
2021-10-18 17:35:59 +0800

    '1'

---
+++ data.py
@@ -0,0 +1,134 @@
+import os
+import numpy as np
+import librosa
+import tensorflow as tf
+from config import hop_length,bins_per_octave
+
+def slice_first_dim(array, slice_size):
+    n_sections = int(np.floor(array.shape[1]/slice_size))
+    has_last_mag = n_sections*slice_size < array.shape[1]
+
+    last_mag = np.zeros(shape=(1, array.shape[0], slice_size, array.shape[2]))
+    last_mag[:,:,:array.shape[1]-(n_sections*slice_size),:] = array[:,n_sections*int(slice_size):,:]
+    
+    if(n_sections > 0):
+        array = np.expand_dims(array, axis=0)
+        sliced = np.split(array[:,:,0:n_sections*slice_size,:], n_sections, axis=2)
+        sliced = np.concatenate(sliced, axis=0)
+        if(has_last_mag): # Check for reminder
+            sliced = np.concatenate([sliced, last_mag], axis=0)
+    else:
+        sliced = last_mag
+    return sliced
+
+def slice_magnitude(mag, slice_size):
+    magnitudes = np.stack([mag], axis=2)
+    return slice_first_dim(magnitudes, slice_size)
+
+def join_magnitude_slices(mag_sliced, target_shape):
+    mag = np.zeros((mag_sliced.shape[1], mag_sliced.shape[0]*mag_sliced.shape[2]))
+    for i in range(mag_sliced.shape[0]):
+        mag[:,(i)*mag_sliced.shape[2]:(i+1)*mag_sliced.shape[2]] = mag_sliced[i,:,:,0]
+    mag = mag[0:target_shape[0], 0:target_shape[1]]
+    return mag
+
+def add_hf(mag, target_shape):
+    rec = np.zeros(target_shape)
+    rec[0:mag.shape[0],0:mag.shape[1]] = mag
+    return rec
+
+def remove_hf(mag):
+    return mag[0:int(mag.shape[0]/2), :]
+
+def forward_cqt(audio,sr):
+    cqt = librosa.cqt(audio, sr=sr,hop_length=hop_length, n_bins=8*bins_per_octave,bins_per_octave=bins_per_octave)
+    mag, phase = librosa.magphase(cqt)
+    return mag, phase
+
+def inverse_cqt(mag, phase, sr):
+    R = mag * phase
+    audio = librosa.icqt(R, sr=sr,hop_length=hop_length,bins_per_octave=bins_per_octave)
+    return audio
+
+def forward_transform(audio, nfft=1024, normalize=True, crop_hf=True):
+    window = np.hanning(nfft)
+    S = librosa.stft(audio, n_fft=nfft, hop_length=int(nfft/2), window=window)
+    mag, phase = librosa.magphase(S) #np.abs(S), np.angle(S)
+    if(crop_hf):
+        mag = remove_hf(mag)
+    if(normalize):
+        mag = 2 * mag / np.sum(window)
+    return mag, phase
+
+def inverse_transform(mag, phase, nfft=1024, normalize=True, crop_hf=True):
+    window = np.hanning(nfft)
+    if(normalize):
+        mag = mag * np.sum(np.hanning(nfft)) / 2
+    if(crop_hf):
+        mag = add_hf(mag, target_shape=(phase.shape[0], mag.shape[1]))
+    R = mag * phase
+    audio = librosa.istft(R, hop_length=int(nfft/2), window=window)
+    return audio
+
+class DataGenerator(tf.keras.utils.Sequence):
+    def __init__(self, origin, target, base_path, batch_size=1, img_dim=(256,256,1), shuffle=True):
+        self.img_dim = img_dim
+        self.batch_size = batch_size
+
+        self.base_path = base_path if(type(base_path) is list) else [base_path]
+
+        self.origin = origin
+        self.target = target
+        self.filenames = self.__get_filenames()
+        assert len(self.filenames) > 0, 'Filenames is empty' 
+
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.filenames) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        filenames = self.filenames[index*self.batch_size:(index+1)*self.batch_size]  # Generate indexes of the batch
+        batch = self.__data_generation(filenames)                                    # Generate data
+        return batch
+
+    def get_empty_batch(self):
+        batch = np.zeros((self.batch_size, *self.img_dim))
+        return batch, batch
+
+    def get_random_batch(self):
+        random_idx = np.random.randint(self.__len__())
+        return self.__getitem__(random_idx)
+
+    def __data_generation(self, filenames):
+        'Generates data containing batch_size samples'                                  
+        x = np.empty((self.batch_size, *self.img_dim))
+        y = np.empty((self.batch_size, *self.img_dim))
+        # Generate data
+        for i, filename in enumerate(filenames):
+            x[i,] = np.load(filename)
+            y[i,] = np.load(filename.replace(self.origin, self.target))
+            
+            # Now images should be scaled in the range [0,1]. Make them [-1,1]
+            x[i,] = x[i,] * 2 - 1
+            y[i,] = y[i,] * 2 - 1
+        return x,y
+
+    def __get_filenames(self):
+        origin_filenames, target_filenames = [],[]
+        for base_path in self.base_path:
+            origin_temp = [os.path.join(base_path, self.origin, f) for f in os.listdir(os.path.join(base_path, self.origin))]
+            target_temp = [os.path.join(base_path, self.target, f) for f in os.listdir(os.path.join(base_path, self.target))]
+            origin_filenames += origin_temp
+            target_filenames += target_temp
+        if(len(origin_filenames) == len(target_filenames)):
+            return origin_filenames
+        return []
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        if self.shuffle:
+            np.random.shuffle(self.filenames)
---
+++ train_stft.py
@@ -0,0 +1,195 @@
+import argparse
+import os
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from config import CHECKPOINT_DIR, IMG_DIM, OUTPUT_PATH, TEST_AUDIOS_PATH,DEFAULT_SAMPLING_RATE , amin
+from data import DataGenerator,forward_transform, inverse_transform,join_magnitude_slices,slice_magnitude
+from losses import discriminator_loss, generator_loss, l1_loss
+from model import Discriminator, Generator
+
+tf.enable_eager_execution()
+
+def generate_audio(prediction, phase, output_name):
+    mag_db = join_magnitude_slices(prediction, phase.shape)
+    mag_db *= 20*np.log1p(1/(1/(2**16)))
+    mag = librosa.db_to_amplitude(mag_db)
+    audio = inverse_transform(mag, phase)
+    librosa.output.write_wav(output_name, audio, sr=DEFAULT_SAMPLING_RATE, norm=True)
+
+def train(data, epochs, batch_size=1, gen_lr=1e-4, disc_lr=1e-4, epoch_offset=0):
+    generator = Generator()
+    discriminator = Discriminator()
+
+    generator_optimizer = tf.keras.optimizers.Adam(gen_lr)
+    discriminator_optimizer = tf.keras.optimizers.Adam(disc_lr)
+
+    model_name = data.origin+'_2_'+data.target+'_stft'
+    checkpoint_prefix = os.path.join(CHECKPOINT_DIR, model_name)
+    
+    # Pretrained model
+    generator_weights = os.path.join(checkpoint_prefix, 'generator.h5')
+    discriminator_weights = os.path.join(checkpoint_prefix, 'discriminator.h5')
+
+    if(not os.path.isdir(checkpoint_prefix)):
+        os.makedirs(checkpoint_prefix)
+    else:
+        if(os.path.isfile(generator_weights)):
+            generator.load_weights(filepath=generator_weights, by_name=True)
+            print('Generator weights restorred from ' + generator_weights)
+
+        if(os.path.isfile(discriminator_weights)):
+            discriminator.load_weights(discriminator_weights, by_name=True)
+            print('Discriminator weights restorred from ' + discriminator_weights)
+
+    epoch_size = data.__len__()
+
+    print()
+    print("Started training with the following parameters: ")
+    print("\tCheckpoints: \t", checkpoint_prefix)
+    print("\tEpochs: \t", epochs)
+    print("\tgen_lr: \t", gen_lr)
+    print("\tdisc_lr: \t", disc_lr)
+    print("\tBatchSize: \t", batch_size)
+    print("\tnBatches: \t", epoch_size)
+    print()
+
+    # Precompute the test input and target for validation
+    audio_input,_ = librosa.load( TEST_AUDIOS_PATH+'//'+data.origin+'.wav', DEFAULT_SAMPLING_RATE)
+    mag_input, phase = forward_transform(audio_input)
+    mag_input = librosa.amplitude_to_db(mag_input, ref=np.min, amin=amin) # amplitude to db
+    mag_input /= 20*np.log1p(1/amin )
+    test_input = slice_magnitude(mag_input, mag_input.shape[0])
+    test_input = test_input * 2 - 1
+
+    audio_target,_ = librosa.load( TEST_AUDIOS_PATH+'//'+data.origin+'.wav', DEFAULT_SAMPLING_RATE)
+    mag_target, _ = forward_transform(audio_target)
+    mag_input = librosa.amplitude_to_db(mag_input, ref=np.min, amin=amin) # amplitude to db
+    mag_target /= 20*np.log1p(1/amin )
+    test_target = slice_magnitude(mag_target, mag_target.shape[0])
+    test_target = test_target * 2 - 1
+
+    gen_mae_list, gen_loss_list ,disc_loss_list = [], [], []
+    
+    test_input,test_target = tf.cast(test_input,tf.float32),tf.cast(test_target,tf.float32) # transfer to float32
+    
+    for epoch in range(epochs):
+        gen_mae_total,gen_loss_total ,disc_loss_total = 0, 0 , 0
+
+        print('Epoch {}/{}'.format((epoch+1)+epoch_offset, epochs+epoch_offset))
+        # Progress Bar
+        progbar = tf.keras.utils.Progbar(epoch_size)
+        for i in range(epoch_size):
+            # Get the data from the DataGenerator
+            input_image, target = data.__getitem__(i) 
+            # transfer to float32
+            input_image,target = tf.cast(input_image,tf.float32),tf.cast(target,tf.float32)
+            with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+                # Generate a fake image
+                gen_output = generator(input_image, training=True)
+                
+                # Train the discriminator
+                disc_real_output = discriminator([input_image, target], training=True)
+                disc_generated_output = discriminator([input_image, gen_output], training=True)
+                
+                # Compute the losses
+                gen_mae = l1_loss(target, gen_output)
+                gen_loss = generator_loss(disc_generated_output, gen_mae)
+                disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+                
+                # Compute the gradients
+                generator_gradients = gen_tape.gradient(gen_loss,generator.trainable_variables)
+                discriminator_gradients = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+                
+                # Apply the gradients
+                generator_optimizer.apply_gradients(zip(generator_gradients, generator.trainable_variables))
+                discriminator_optimizer.apply_gradients(zip(discriminator_gradients, discriminator.trainable_variables))
+
+                # Update the progress bar and the totals
+                gen_mae = gen_mae.numpy()
+                gen_loss = gen_loss.numpy()
+                disc_loss = disc_loss.numpy()
+                
+                
+                gen_mae_total += gen_mae
+                gen_loss_total += gen_loss
+                disc_loss_total += disc_loss
+
+                progbar.add(1, values=[
+                                        ("gen_mae", gen_mae), 
+                                        ("gen_loss", gen_loss), 
+                                        ("disc_loss", disc_loss)
+                                    ])
+        
+        # Write training history 
+        gen_mae_list.append(gen_mae_total/epoch_size)
+        gen_loss_list.append(gen_loss_total/epoch_size)
+        disc_loss_list.append(disc_loss_total/epoch_size)
+
+        history = pd.DataFrame({
+                                'gen_mae': gen_mae_list,
+                                'gen_loss': gen_loss_list,
+                                'disc_loss': disc_loss_list,
+                                })
+        history.to_csv(os.path.join(checkpoint_prefix, 'history.csv') , header='column_names')
+
+        # Generate audios and save spectrograms for the entire audios
+        epoch_output = os.path.join(OUTPUT_PATH, model_name, str((epoch+1)+epoch_offset).zfill(3))
+        # init directory
+        if(not os.path.isdir(epoch_output)):
+            os.makedirs(epoch_output)
+
+        prediction = generator(test_input, training=False)
+        prediction = (prediction + 1) / 2
+
+        # save image
+        plt.imsave(os.path.join(epoch_output, 'spectrogram')+'_'+'input'+'.png', np.flip((((test_input + 1) / 2)[0,:,:,0] + 1) / 2, axis=0)) 
+        plt.imsave(os.path.join(epoch_output, 'spectrogram')+'_'+'true'+'.png', np.flip((((test_target + 1) / 2)[0,:,:,0] + 1) / 2, axis=0)) 
+        plt.imsave(os.path.join(epoch_output, 'spectrogram')+'_'+'pred'+'.png', np.flip((prediction[0,:,:,0] + 1) / 2, axis=0))
+
+        generate_audio(prediction, phase, os.path.join(epoch_output, 'audio.wav'))
+        print('Epoch outputs saved in ' + epoch_output)
+
+        # Save the weights
+        generator.save_weights(generator_weights)
+        discriminator.save_weights(discriminator_weights)
+        print('Weights saved in ' + checkpoint_prefix)
+
+        # Callback at the end of the epoch for the DataGenerator
+        data.on_epoch_end()
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--dataset_path', required=False,default='D://4550//Timbre-Transfer//data//stft_features//')
+    ap.add_argument('--origin', required=False,default='keyboard_acoustic')
+    ap.add_argument('--target', required=False,default='string_acoustic')
+    ap.add_argument('--gpu', required=False, default='0')
+    ap.add_argument('--epochs', required=False, default=20)
+    ap.add_argument('--epoch_offset', required=False, default=0)
+    ap.add_argument('--batch_size', required=False, default=5)
+    ap.add_argument('--gen_lr', required=False, default=5e-6)
+    ap.add_argument('--disc_lr', required=False, default=5e-7)
+    args = ap.parse_args()
+
+    # Select which GPU to use and enable mixed precision
+    print('Using GPU: '+ args.gpu)
+    os.environ["CUDA_DEVICE_ORDER"] = 'PCI_BUS_ID'
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu
+    os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
+
+    dataset = DataGenerator(origin=args.origin, 
+                        target=args.target,
+                        base_path=args.dataset_path,
+                        batch_size=int(args.batch_size),
+                        img_dim=IMG_DIM)
+    train(
+        data=dataset, 
+        epochs=int(args.epochs), 
+        batch_size=int(args.batch_size), 
+        gen_lr=float(args.gen_lr), 
+        disc_lr=float(args.disc_lr), 
+        epoch_offset=int(args.epoch_offset)
+        )
